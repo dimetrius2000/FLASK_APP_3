587e7b3e9dd52ee7b2dd1f023a6a412aee2234fe
dimetrius2000 <dimetris311200@mail.ru>
2023-06-07 20:23:37 +0300

    Initial commit

--- blog/app.py
+++ blog/app.py
@@ -1,73 +1,68 @@
-import time
-from flask import Flask,request
-from flask import g
-from werkzeug.exceptions import BadRequest
+from combojsonapi.event import EventPlugin
+from combojsonapi.permission import PermissionPlugin
+from combojsonapi.spec import ApiSpecPlugin
+from flask import Flask
 
-app = Flask(__name__)
-@app.route("/")
-def index():
-    return "Hello web!"
-
-@app.route("/greet/<name>/")
-def greet_name(name: str):
-    return f"Hello {name}!"
+from blog import commands
+from blog.extensions import db, login_manager, migrate, csrf, admin, api
+from blog.models import User
 
 
-@app.route("/user/")
-def read_user():
-    name = request.args.get("name")
-    surname = request.args.get("surname")
-    return f"User {name or '[no name]'} {surname or '[no surname]'}"
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.from_object('blog.config')
 
-@app.route("/status/", methods=["GET", "POST"])
-def custom_status_code():
-    if request.method == "GET":
-        return """\
-        To get response with custom status code
-        send request using POST method
-        and pass `code` in JSON body / FormData
-        """
-    print("raw bytes data:", request.data)
-    if request.form and "code" in request.form:
-        return "code from form", request.form["code"]
-    if request.json and "code" in request.json:
-        return "code from json", request.json["code"]
-    return "", 204
+    register_extensions(app)
+    register_blueprints(app)
+    register_commands(app)
+    return app
 
 
+def register_extensions(app):
+    db.init_app(app)
+    migrate.init_app(app, db, compare_type=True)
+    csrf.init_app(app)
+    admin.init_app(app)
+    api.plugins = [
+        EventPlugin(),
+        PermissionPlugin(),
+        ApiSpecPlugin(
+            app=app,
+            tags={
+                'Tag': 'Tag API',
+                'User': 'User API',
+                'Author': 'Author API',
+                'Article': 'Article API',
+            }
+        ),
+    ]
+    api.init_app(app)
 
-@app.before_request
-def process_before_request():
-    """
-    Sets start_time to `g` object
-    """
-    g.start_time = time()
-@app.after_request
-def process_after_request(response):
-    """
-    adds process time in headers
-    """
-    if hasattr(g, "start_time"):
-        response.headers["process-time"] = time() - g.start_time
-    return response
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
-@app.route("/power/")
-def power_value():
-    x = request.args.get("x") or ""
-    y = request.args.get("y") or ""
-    if not (x.isdigit() and y.isdigit()):
-        app.logger.info("invalid values for power: x=%r and y=%r", x, y)
-        raise BadRequest("please pass integers in `x` and `y` query params")
-    x = int(x)
-    y = int(y)
-    result = x ** y
-    app.logger.debug("%s ** %s = %s", x, y, result)
-    return str(result)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
-@app.route("/divide-by-zero/")
-def do_zero_division():
-    return 1 / 0
 
-@app.errorhandler(ZeroDivisionError)
-def handle_zero_division_error(error):
-    print(error) # prints str version of error: 'division by zero' app.logger.exception("Here's traceback for zero division error") return "Never divide by zero!", 400
+def register_blueprints(app: Flask):
+    from blog.auth.views import auth
+    from blog.user.views import user
+    from blog.author.views import author
+    from blog.articles.views import article
+    from blog.api.views import api_blueprint
+    from blog import admin
+
+    app.register_blueprint(user)
+    app.register_blueprint(auth)
+    app.register_blueprint(author)
+    app.register_blueprint(article)
+    app.register_blueprint(api_blueprint)
+
+    admin.register_views()
+
+
+def register_commands(app: Flask):
+    app.cli.add_command(commands.create_init_user)
+    app.cli.add_command(commands.create_init_tags)
